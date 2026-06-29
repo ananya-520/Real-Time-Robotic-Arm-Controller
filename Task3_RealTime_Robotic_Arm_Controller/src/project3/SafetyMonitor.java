96c8639b70a3f47d8007bf1412a91a3739b3b06f
Ananya <153264820+ananya-520@users.noreply.github.com>
2026-06-29 23:25:22 +0000

    Task 3 Upload

---
+++ Task3_RealTime_Robotic_Arm_Controller/src/project3/SafetyMonitor.java
@@ -0,0 +1,44 @@
+package project3;
+/**
+* High-priority thread representing a safety monitoring system.
+* Continuously runs and accesses the shared MotorController resource.
+*/
+public class SafetyMonitor extends Thread {
+	
+	private MotorController controller;
+	public long waitTime;
+	
+	/**
+	 * Constructs a SafetyMonitor thread with a shared MotorController.
+	 *
+	 * @param controller shared MotorController instance
+	 */
+	public SafetyMonitor(MotorController controller) {
+		setName("Safety Monitor");
+		this.controller = controller;
+	}
+	
+	/**
+	 * Continuously executes high-priority monitoring tasks and accesses the shared MotorController resource.
+	 */
+	public void run() {
+		while (true) {
+			try {
+				Thread.sleep(100);
+			}
+			catch (InterruptedException e){
+				
+			}
+			
+			System.out.println(MotorController.getTimeStamp() + ": [EXECUTION] Safety Monitor requesting resource");
+			
+			long start = System.nanoTime();
+			controller.accessController(getName());
+			waitTime = System.nanoTime() - start;
+			
+			System.out.println(MotorController.getTimeStamp() + ": [METRIC] Safety Monitor Waiting Time = " + (waitTime/1000000.0) + " ms");
+		}
+		
+	}
+  
+}
